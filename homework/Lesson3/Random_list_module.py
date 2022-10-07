4121db7b86670a3934ef24e2180ad1a98037d3dc
Senchenko_Vadim <Senevad@yandex.ru>
2022-10-07 14:10:45 +0300

    добавил модуль рандомных списков

---
+++ homework/Lesson3/Random_list_module.py
@@ -0,0 +1,8 @@
+from random import randint
+
+def Random_list (N,min,max):
+    list_N = []
+    for i in range(N):
+        list.append(list_N, randint(min,max))
+    print ("Задан список:",list_N)
+    return list_N
