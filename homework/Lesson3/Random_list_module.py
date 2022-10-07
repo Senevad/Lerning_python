f29ee972be44fb8d6834b79b899e797f0c0ecbb0
Senchenko_Vadim <Senevad@yandex.ru>
2022-10-07 14:23:20 +0300

    добавил в мудуль функцию с веществеными числами

--- homework/Lesson3/Random_list_module.py
+++ homework/Lesson3/Random_list_module.py
@@ -1,4 +1,4 @@
-from random import randint
+from random import randint, random
 
 def Random_list (N,min,max):
     list_N = []
@@ -6,3 +6,10 @@ def Random_list (N,min,max):
         list.append(list_N, randint(min,max))
     print ("Задан список:",list_N)
     return list_N
+
+def Random_list_float (N,min,max):
+    list_float = []
+    for i in range(N):
+        list.append(list_float, (randint(min,max) + round(random(),4)))
+    print ("Задан список:",list_float)
+    return list_float
