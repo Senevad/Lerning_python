ddeb7e19f5c6e96144abf7d9c65dd45612e0e334
Senchenko_Vadim <Senevad@yandex.ru>
2022-10-11 09:22:08 +0300

    модифицировал задание 4

--- homework/Lesson4/Task4/Task4.py
+++ homework/Lesson4/Task4/Task4.py
@@ -7,20 +7,23 @@
 from Random_list_module import Random_list
 
 k = int(input("Введите k:"))
-list_data = Random_list(k + 1,0,5)
-list_result = []
-result_task = ""
-for i in range(k+1):
-    if list_data[i] != 0:
-        if i == 0:
-            list_result.insert(0,f"{list_data[i]}")
-        elif i == 1:
-            list_result.insert(0,f"{list_data[i]}X")
-        else:
-            list_result.insert(0,f"{list_data[i]}X^{i}")
-for i in range(len(list_result)-1):
-    result_task += list_result[i] + " + "
-result_task += list_result[-1] + " = 0"
+list_rand_data = Random_list(k + 1,0,5)
+
+def mulp_in_string(list_data):
+    list_result = []
+    result_task = ""
+    for i in range(len(list_data)):
+        if list_data[i] != 0:
+            if i == 0:
+                list_result.insert(0,f"{list_data[i]}")
+            elif i == 1:
+                list_result.insert(0,f"{list_data[i]}X")
+            else:
+                list_result.insert(0,f"{list_data[i]}X^{i}")
+    for i in range(len(list_result)-1):
+        result_task += list_result[i] + " + "
+    result_task += list_result[-1] + " = 0"
+    return result_task
 
 with open("homework/Lesson4/Task4/Task4_file.txt", "w") as data:
-    data.write(result_task)
+    data.write(mulp_in_string(list_rand_data))
