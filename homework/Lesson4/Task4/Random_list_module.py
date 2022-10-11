3028844f8b0c477d451d137090858decdd39f8f0
Senchenko_Vadim <Senevad@yandex.ru>
2022-10-11 08:06:17 +0300

    выполнил задание 4

---
+++ homework/Lesson4/Task4/Random_list_module.py
@@ -0,0 +1,15 @@
+from random import randint, random
+
+def Random_list (N,min,max):
+    list_N = []
+    for i in range(N):
+        list.append(list_N, randint(min,max))
+    print ("Задан список:",list_N)
+    return list_N
+
+def Random_list_float (N,min,max):
+    list_float = []
+    for i in range(N):
+        list.append(list_float, (randint(min,max) + round(random(),4)))
+    print ("Задан список:",list_float)
+    return list_float
---
+++ homework/Lesson4/Task4/Task4.py
@@ -0,0 +1,26 @@
+# Задана натуральная степень k. Сформировать случайным образом список коэффициентов (значения от 0 до 100) многочлена и записать в файл многочлен степени k.
+
+# Пример:
+
+# - k=2 => 2*x² + 4*x + 5 = 0 или x² + 5 = 0 или 10*x² = 0
+
+from Random_list_module import Random_list
+
+k = int(input("Введите k:"))
+list_data = Random_list(k + 1,0,5)
+list_result = []
+result_task = ""
+for i in range(k+1):
+    if list_data[i] != 0:
+        if i == 0:
+            list_result.insert(0,f"{list_data[i]}")
+        elif i == 1:
+            list_result.insert(0,f"{list_data[i]}X")
+        else:
+            list_result.insert(0,f"{list_data[i]}X^{i}")
+for i in range(len(list_result)-1):
+    result_task += list_result[i] + " + "
+result_task += list_result[-1] + " = 0"
+
+with open("homework/Lesson4/Task4/Task4_file.txt", "w") as data:
+    data.write(result_task)
