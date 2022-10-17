77686922643243388635ccaaf1d58397313ced6d
Senchenko_Vadim <Senevad@yandex.ru>
2022-10-18 01:46:49 +0300

    1 улучшеное задание

---
+++ homework/Lesson6/Task1.py
@@ -0,0 +1,25 @@
+# Напишите программу, которая найдёт произведение пар чисел списка.
+#  Парой считаем первый и последний элемент, второй и предпоследний и т.д.
+
+# Пример:
+
+# - [2, 3, 4, 5, 6] => [12, 15, 16];
+# - [2, 3, 5, 6] => [12, 15]
+
+# from Random_list_module import Random_list
+
+# list_N = Random_list(5,0,10)
+
+# list_duo_mulp = []
+# for i in range(0,int((len(list_N)+1)/2)):
+#     list.append(list_duo_mulp,list_N[i]*(list_N[len(list_N)-i-1]))
+# print("Результат:", list_duo_mulp)
+
+# улучшение задания 2 лекиции 3 с помощью новых функций
+
+from Random_list_module import Random_list
+
+list_N = Random_list(5,0,10)
+
+list_duo_mulp = [list_N[i]*(list_N[len(list_N)-i-1]) for i in range(0,int((len(list_N)+1)/2))]
+print("Результат:", list_duo_mulp)
