61b3e0f9b18d8b665901ba3e4d48725639b862ac
Senchenko_Vadim <Senevad@yandex.ru>
2022-11-11 18:04:53 +0300

    выполнил задание 2

--- homework/Lesson9/Task2/Main.py
+++ homework/Lesson9/Task2/Main.py
@@ -1,5 +1,40 @@
-# Задание в группах: Создать телефонный справочник с возможностью импорта и экспорта данных в нескольких форматах.
+# 2)Прикрутить бота к задачам с предыдущего семинара:
+# Создать калькулятор( можно добавить работу с рациональными и комплексными числами), организовать меню, добавив в неё систему логирования
 
-from Controler import run_programm
+# Имя бота: python_test_homework_bot
 
-run_programm()
+
+from telegram import Update
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+
+
+async def hello(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    await update.message.reply_text(f'Hello {update.effective_user.first_name}')
+
+async def help(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    await update.message.reply_text(f'/hello\n/sum\n/dif')
+
+async def sum(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    msg = update.message.text
+    items = msg.split()
+    x = int(items[1])
+    y = int(items[2])
+    await update.message.reply_text(f'{x} + {y} = {x + y}')
+
+async def dif(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    msg = update.message.text
+    items = msg.split()
+    x = int(items[1])
+    y = int(items[2])
+    await update.message.reply_text(f'{x} - {y} = {x - y}')
+
+app = ApplicationBuilder().token("5788610930:AAHHW-FsfifxsGD7w6l6ah5e4FvLo3s7BGY").build()
+
+app.add_handler(CommandHandler("hello", hello))
+app.add_handler(CommandHandler("sum",sum))
+app.add_handler(CommandHandler("dif",dif))
+app.add_handler(CommandHandler("help",help))
+
+print("сервер запущен")
+
+app.run_polling()
--- homework/Lesson9/Task2/Read_input.py
+++
@@ -1,9 +0,0 @@
-def read_txt(filename: str) -> list:
-    data = []
-    fields = ["Фамилия", "Имя", "Телефон", "Описание"]
-    with open(filename, 'r', encoding='utf-8') as fin:
-        for line in fin:
-            record = dict(zip(fields, line.strip().split(' ')))
-            data.append(record)
-
-    return data
